974f4f1f05d0646e3d3841d4a6714483368786bd
Nikita Velichko <velichkonikita@gmail.com>
2020-11-26 22:46:24 +0300

    Created a project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Nikita Velichko on 11/26/20.
+//  Copyright © 2020 Nikita Velichko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
