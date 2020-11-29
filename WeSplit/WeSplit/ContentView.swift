371ef2f3a06fc4cc0912e2a3ea2e8b169f9323da
Nikita Velichko <velichkonikita@gmail.com>
2020-11-29 16:25:14 +0300

    Finished App

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        return totalPerPerson * Double(numberOfPeople + 2)
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+    NavigationView {
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100) {
+                        Text("\($0) People")
+                    }
+                }
+            }
+            
+            Section(header: Text("How much tip do you want to leave?")) {
+                Picker("Tip Percentage", selection: $tipPercentage) {
+                    ForEach(0 ..< tipPercentages.count) {
+                        Text("\(self.tipPercentages[$0])%")
+                    }
+                }
+            .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("Amount Per Person")) {
+                Text("$\(totalPerPerson, specifier: "%.2f")")
+            }
+            Section {
+                Text("\(totalAmount, specifier: "%.2f")")
+            }
+        }
+    .navigationBarTitle(Text("WeSplit"))
+    }
     }
 }
 
